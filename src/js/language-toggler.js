ef206eb00ea3c87c914906c5999c57bc0d7cc44b
Denys Volodko <babyleejane@gmail.com>
2024-06-21 17:32:40 +0300

    brokerage service translate

--- src/js/language-toggler.js
+++ src/js/language-toggler.js
@@ -1,16 +1,18 @@
 (() => {
-	const languageTogglerButton = document.querySelector(
-		".header-language-select-btn",
+	const languageTogglerButton = document.querySelectorAll(
+		"[data-btn='language']",
 	);
 
-	if (window.location.pathname === "/") {
-		const newPathName = "/en/index";
-		languageTogglerButton.attributes.href.value = newPathName;
-	} else if (window.location.pathname.includes("/en")) {
-		const newPathName = window.location.pathname.replace("/en", "");
-		languageTogglerButton.attributes.href.value = newPathName;
-	} else {
-		const newPathName = `/en${window.location.pathname}`;
-		languageTogglerButton.attributes.href.value = newPathName;
-	}
+	languageTogglerButton.forEach(button => {
+		if (window.location.pathname === "/") {
+			const newPathName = "/en/index";
+			button.attributes.href.value = newPathName;
+		} else if (window.location.pathname.includes("/en")) {
+			const newPathName = window.location.pathname.replace("/en", "");
+			button.attributes.href.value = newPathName;
+		} else {
+			const newPathName = `/en${window.location.pathname}`;
+			button.attributes.href.value = newPathName;
+		}
+	});
 })();
